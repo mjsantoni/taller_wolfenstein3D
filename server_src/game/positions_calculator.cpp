8c07c60996df45d9fa2ee0c990e1efd0f492c955
Mauro Santoni <msantoni@fi.uba.ar>
2021-01-13 19:34:41 -0300

    Fixea positionCalc: movimiento ahora funciona bien 100%

--- server_src/game/positions_calculator.cpp
+++ server_src/game/positions_calculator.cpp
@@ -20,24 +20,22 @@ PositionsCalculator::straightLine(Coordinate old_pos, Coordinate new_pos) {
         bool it_on_x = abs(y_new - y_old) < abs(x_new - x_old);
         double m = (it_on_x) ? ((double) (y_new - y_old) / (x_new - x_old)) :
                    ((double) (x_new - x_old) / (y_new - y_old));
-        double b = (double) y_old - (m * x_old);
+        double b = (it_on_x) ? ((double) y_old - (m * x_old)) :
+                               ((double) x_old - (m * y_old));
         int axis_old = it_on_x ? x_old : y_old;
         int axis_new = it_on_x ? x_new : y_new;
         //Si se quiere algo mucho mas aproximado no hacer el for del else
         //y solo hacer el primer for. Esto es mucho mas overfiteado.
+
         for (int i = axis_old; (axis_old < axis_new) ? i <= axis_new : i >= axis_new; (axis_old < axis_new) ? i++ : i--) {
             Coordinate pos(-1, -1);
             if (it_on_x) {
-                //std::cout << "AntesITX: " << pos.x << " - Y: " << pos.y << "\n";
                 pos.x = i;
                 pos.y = std::round(m * i + b);
-                //std::cout << "DespsITX: " << pos.x << " - Y: " << pos.y << "\n";
             }
             else {
-                //std::cout << "AntesX: " << pos.x << " - Y: " << pos.y << "\n";
-                pos.x = std::round(m * i + b); // esto esta roto
+                pos.x = std::round(m * i + b);
                 pos.y = i;
-                //std::cout << "DespsX: " << pos.x << " - Y: " << pos.y << "\n";
             }
             items.push_back(pos);
         }
