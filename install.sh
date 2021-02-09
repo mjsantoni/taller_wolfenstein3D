53380d0b83ab6c1b2c27720f7d0f8374dabb6310
Francisco <fbertolotto@fi.uba.ar>
2021-02-08 22:55:43 -0300

    Update script

--- install.sh
+++ install.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 #SDL2
-sudo apt install libsdl2-dev libsdl2-2.0-0 -y
-sudo apt install libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 -y
-sudo apt install libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 -y
+sudo apt-get install libsdl2-dev libsdl2-2.0-0 -y
+#SDL Image
+sudo apt-get install libjpeg-dev libwebp-dev libtiff5-dev libsdl2-image-dev libsdl2-image-2.0-0 -y
+#SDL Mixer
+sudo apt-get install libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 -y
+#SDL Fonts
 sudo apt-get install libsdl2-ttf-dev
 
 #QT
 sudo apt-get install qt5-default
 
+#LUA
+sudo apt-get install lua5.3 liblua5.3-dev
+
 mkdir build
 cd build
 
 #yaml
-git clone https://github.com/jbeder/yaml-cpp
-cd yaml-cpp
+wget https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.3.zip
+unzip yaml-cpp-0.6.3.zip
+cd yaml-cpp-yaml-cpp-0.6.3/
 mkdir build
 cd build
 cmake ..
-make
-sudo make
+make install
+sudo make install
 cd ..
