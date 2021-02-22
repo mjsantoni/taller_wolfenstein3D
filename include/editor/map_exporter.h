9e67699637ff938d68d49dfbd73afe3e7c8a5615
Agustin <alopezn@fi.uba.ar>
2021-02-22 18:06:55 -0300

    Mejoras visuales en el menu inicio

--- include/editor/map_exporter.h
+++ include/editor/map_exporter.h
@@ -7,13 +7,19 @@
 #include "common/coordinate.h"
 #include "yaml-cpp/yaml.h"
 
+/* This class is the responsable of parsing the positions of all the textures*/
 class MapExporter {
     std::string width;
     std::string height;
     std::unordered_map<std::string, std::vector<std::pair<int, int>>>& positions;
     YAML::Emitter out;
+
 public:
+
     MapExporter(std::string &_width, std::string &_height, std::unordered_map<std::string, std::vector<std::pair<int, int>>>& _positions);
+
+    /* Returns the char* containing all the info from the map
+     * ready to load into a file*/
     const char *getMapParsed();
 };
 
